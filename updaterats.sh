826e1cfc08dd2bb220b0b1795684e1dc3487d069
Stefan Eekhof <stefaneekhof@aerialworks.ddns.net>
2021-03-29 16:08:33 +0200

    Update dependencies

--- updaterats.sh
+++ updaterats.sh
@@ -3,7 +3,7 @@ git="https://aerialworks.ddns.net/ASF/RATS.git"
 dir="$(pwd)"
 
 echo 'Updating RaTs! installation for libraries...'
-rm -rf libraries/
+rm -rf libraries/ 2>/dev/null
 
 echo Cloning git repository...
 tmpdir="/tmp/build-rats-connective-http-standalone/$(date "+%s-%N")"
@@ -22,7 +22,8 @@ function exitmeth() {
 
 function execute() {
     gradle installation || return $?
-    mkdir "$dir/libraries"
+    
+    if [ ! -d "$dir/libraries" ]; then mkdir "$dir/libraries"; fi
     cp -r "build/Installations/"*.jar "$dir/libraries"
 }
 
